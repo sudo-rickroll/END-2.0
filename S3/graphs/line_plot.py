1ec40b199e78b8445dfb0e7c0f30a324c5d074ee
Rangasai <65642947+sudo-rickroll@users.noreply.github.com>
2021-05-22 23:08:56 +0530

    Add files via upload

--- S3/graphs/line_plot.py
+++ S3/graphs/line_plot.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 
 def plot(plots, labels, xlabel, ylabel, title):
     for index, metric in enumerate(plots):
-        plt.plot(metric, label = labels[index])
+        plt.plot(metric[0], metric[1], label = labels[index])
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.legend(bbox_to_anchor=(1,1))
+    plt.legend(bbox_to_anchor=(1,1), loc = 'upper left')
     plt.title(title)
+    plt.savefig(f'./images/{title} Graph.jpg', bbox_inches='tight')
     plt.show()
+    
     
